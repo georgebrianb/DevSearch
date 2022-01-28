510233f6440ab531d2de9b69dcd415c280f8a68c
georgebrianb <george_brianb@yahoo.com>
2022-01-28 12:35:36 +0200

    added first signal

--- users/models.py
+++ users/models.py
@@ -1,8 +1,10 @@
-
 from django.db import models
 from django.contrib.auth.models import User
 
 import uuid
+
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
 
 class Profile(models.Model):
   # on_delete=models.CASCADE => everytime a use is deleted, the associated 
@@ -34,6 +36,7 @@ class Profile(models.Model):
   def __str__(self):
     return str(self.user.username)
   
+
   
   
   
@@ -50,3 +53,7 @@ class Skill(models.Model):
 
   def __str__(self):
     return str(self.name)
+  
+  
+  
+  
